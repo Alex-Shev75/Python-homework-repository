9ca13607e6a0987040c909182d855cead8ff8607
Alex <a.shev4uk1975@gmail.com>
2022-12-10 23:50:52 +0200

    hw3-done

---
+++ hw3/hw-lesson-3.py
@@ -0,0 +1,24 @@
+print('I love Python\n' * 42)
+
+age_in_month = (47 * 12)
+age_in_years = str(age_in_month // 12)
+
+name = 'Alex'
+
+my_age = ('My name is ' + name + ' I`m ' + age_in_years + ' years old')
+print(f'{my_age} \n')
+
+num = 1
+print(f'{num} > 2    - is {num > 2}')
+print(f'{num} < 5    - is {num < 5}')
+print(f'{num} == 1   - is {num == 1}')
+print(f'{num} >= 0.5 - is {num >= .5}')
+print(f'{num} <= 2   - is {num <= 2}')
+print(f'{num} != 10  - is {num != 10} \n')
+
+a = 2
+b = 5
+c = 6
+d = str(a) + str(b) + str(c)
+print(d)
+
