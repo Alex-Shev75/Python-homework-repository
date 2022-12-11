ac41cedd3392011ae1048197e12bb7245801a52a
Alex <a.shev4uk1975@gmail.com>
2022-12-11 22:48:06 +0200

    hw4-fixed

--- hw4/hw-lesson-4.py
+++ hw4/hw-lesson-4.py
@@ -1,13 +1,11 @@
 """
 Homework-4: text type, even/odd, length
 """
-
 text = input('Please enter a word or a number - ')
 a = text
 if text.isdigit():
     text = int(text)
-    x = text % 2
-    if x == 0:
+    if text % 2 == 0:
         print(f'You entered the even number - {text}')
     else:
         print(f'You entered the odd number - {text}')
