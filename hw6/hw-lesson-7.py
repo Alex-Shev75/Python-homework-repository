9811133fcdd76bb854d0c0f41744b15c37be2307
Alex <a.shev4uk1975@gmail.com>
2022-12-24 18:53:28 +0200

    hw7-added

--- hw6/hw-lesson-7.py
+++ hw6/hw-lesson-7.py
@@ -15,15 +15,15 @@ while True:
                          '5 - See the number of subscribes\n===='
                          '\nEnter command number - ')
     for item in user_command:
-        if item.isdigit():
-            item = int(item)
-            if item == 5:
+        if item.isalnum():
+            item = str(item)
+            if item == '5':
                 print(f'\n====\nThere are {len(my_phone_book)} subscribers in your phone book\n====\n')
-            elif item == 4:
+            elif item == '4':
                 print('\nSubscriber list:\n====\n')
                 for subscriber in my_phone_book:
                     print(subscriber)
-            elif item == 3:
+            elif item == '3':
                 while True:
                     sub_command_view = input('\n====\nChoose subscriber: - ')
                     command_view = sub_command_view
@@ -35,7 +35,7 @@ while True:
                         print('\n====\nSubscriber not found, select from the list:\n====\n')
                         for subscriber in my_phone_book:
                             print(subscriber)
-            elif item == 2:
+            elif item == '2':
                 while True:
                     sub_command_delete = input('\n====\nSelect the subscribers to be deleted: - ')
                     command_delete = sub_command_delete
@@ -50,14 +50,25 @@ while True:
                         print('\n====\nSubscriber not found, select from the list:\n====\n')
                         for subscriber in my_phone_book:
                             print(subscriber)
-            elif item == 1:
-                name_input = input('\n====\nEnter name - ')
-                name = name_input
-                number_input = input('\n====\nEnter phone number - ')
-                number = number_input
-                my_phone_book[name] = number
-                print(f'\n===\n`{name}` - is added to your phone book')
-        else:
-            print('\n====\nCommand not found, please try again\n====')
-            break
+            elif item == '1':
+                while True:
+                    user_add = input('====\nEnter name - ')
+                    if len(user_add) > 0:
+                        if user_add not in my_phone_book.keys():
+                            number_input = input('====\nEnter phone number - ')
+                            if len(number_input) >= 3:
+                                my_phone_book[user_add] = number_input
+                                print(f'====\n{user_add} added in your phone book\n====')
+                                break
+                            else:
+                                print('===\nNumber must contain min 3 symbols\n====')
+                        else:
+                            print(f'====\n{user_add} is already in your phone book\n====')
+                            for subscriber in my_phone_book:
+                                print(subscriber)
+                    else:
+                        print('===\nName must contain min 1 symbol\n====')
+                    print('Repeat input')
 
+            else:
+                print('\n====\nCommand not found, please try again\n====')
