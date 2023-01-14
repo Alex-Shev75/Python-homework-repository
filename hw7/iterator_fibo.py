739f7927c921a04b765a5fc068196d763ad58f99
Alex <a.shev4uk1975@gmail.com>
2023-01-14 17:04:28 +0200

    iterator_fibo.py

--- hw7/iterator_fibo.py
+++ hw7/iterator_fibo.py
@@ -1,15 +1,15 @@
 class Fibo:
-    def __init__(self, fibo_max=0):
+    def __init__(self, fibo_googol=pow(10, 1000)):
         self.a = 0
         self.b = 1
         self.c = 0
-        self.fibo_max = fibo_max
+        self.fibo_googol = fibo_googol
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self.c < self.fibo_max:
+        if self.c < self.fibo_googol:
             self.c = self.a
             self.a = self.b
             self.b = self.c + self.a
@@ -18,28 +18,15 @@ class Fibo:
             raise StopIteration
 
 
-sq = Fibo(int(input('====\nEnter the maximum value for sequence - ')))
+sq = Fibo()
 lst_sq = list(sq)
-lst_sq.pop()
-print(*lst_sq, sep=', ')
+max_index = (lst_sq.index(lst_sq[-1]))
+print(f'====\nThis sequence consists of {max_index} numbers')
 while True:
-    sel = int(input('====\nEnter command:\n'
-                    '1 - to get the value by index\n'
-                    '2 - to get the index by value\n'
-                    '- '))
-    if sel == 1:
-        index = int(input('====\nEnter index - '))
-        if index < len(lst_sq):
-            print(f'Index `{index}` corresponds number `{lst_sq[index]}`')
-            break
-        else:
-            print('Index out of sequence')
-    elif sel == 2:
-        number = int(input('====\nEnter value - '))
-        if number in lst_sq:
-            print(f'Number `{number} corresponds index `{lst_sq.index(number)}`')
-            break
-        else:
-            print('The number is not in the sequence')
+    index = int(input('====\nEnter index - '))
+    if index <= max_index:
+        print(f'====\nIndex `{index}` corresponds number {lst_sq[index]}')
+        print(f'===\nThis number has {len(str(lst_sq[index]))} digits')
+        break
     else:
-        print(False)
+        print('====\nIndex out of sequence')
